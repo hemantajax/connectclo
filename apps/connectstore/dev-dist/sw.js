e7d559eefdc6a363be5c0a90232c9fb0146faf8d
Hemant <hemant.ajax@gmail.com>
2025-09-11 19:07:53 +0530

    header logo update

--- apps/connectstore/dev-dist/sw.js
+++ apps/connectstore/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-c62ba2b5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tpndfrrbkmg"
+    "revision": "0.5ehh7o16gao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- libs/components/src/layout/Header/Header.tsx
+++ libs/components/src/layout/Header/Header.tsx
@@ -5,6 +5,20 @@ interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = ({ className = '' }) => {
+  const logoStyle: React.CSSProperties = {
+    background:
+      'linear-gradient(135deg, #00d2ff 0%, #3a7bd5 50%, #00d2ff 100%)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundClip: 'text',
+    fontSize: '1.8rem',
+    fontWeight: '900',
+    letterSpacing: '0.1em',
+    textShadow: '0 0 30px rgba(0, 210, 255, 0.3)',
+    fontFamily: '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif',
+    textTransform: 'uppercase' as const,
+  };
+
   return (
     <header role="banner">
       <nav
@@ -14,12 +28,9 @@ export const Header: React.FC<HeaderProps> = ({ className = '' }) => {
         <div className="container-fluid px-4">
           {/* Logo/Brand */}
           <div className="navbar-brand d-flex align-items-center">
-            <img
-              src="https://storagefiles.clo-set.com/public/connect/common/connect-desktop-header-bi.svg"
-              alt="CLO-SET CONNECT - Go to homepage"
-              height="40"
-              className="d-inline-block align-text-top"
-            />
+            <span style={logoStyle} className="d-inline-block">
+              H<span style={{ color: '#00ff88' }}>CONNECT</span>
+            </span>
           </div>
         </div>
       </nav>
